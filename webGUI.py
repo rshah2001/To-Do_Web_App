6b2a2bd66c017fcdd046819f1d3738432b094a4b
Rishil Shah <90150116+rshah2001@users.noreply.github.com>
2024-07-07 15:00:50 -0400

    Update webGUI.py

--- webGUI.py
+++ webGUI.py
@@ -10,8 +10,7 @@ def add_todo():
 
 
 st.title("My To Do App")
-st.subheader("This is my todo app")
-st.write("this app is to increase your productivity ")
+st.write("Get Things Done, One Task at a Time: Unleash Your Productivity")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
